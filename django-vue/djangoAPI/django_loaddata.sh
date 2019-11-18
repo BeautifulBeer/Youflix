f9f5f0cd4ad5e6ca04afaadc8c4f88cbda71382e
BeautifulBeer <dbsrhksdnd@gmail.com>
2019-11-18 16:27:38 +0000

    * Django 의 데이터베이스를 만드는 명령어들을 .sh 파일로 병합 * requirements.txt 파일에 numba 추가

---
+++ django-vue/djangoAPI/django_loaddata.sh
@@ -0,0 +1,14 @@
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata ./json/collection.json
+python manage.py loaddata ./json/company.json
+python manage.py loaddata ./json/country.json
+python manage.py loaddata ./json/genre.json
+python manage.py loaddata ./json/keyword.json
+python manage.py loaddata ./json/language.json
+python manage.py loaddata ./json/movie.json
+python manage.py loaddata ./json/crew.json
+python manage.py loaddata ./json/cast.json
+python manage.py loaddata ./json/user_profile_cluster.json
+python manage.py loaddata ./json/rating.json
+
